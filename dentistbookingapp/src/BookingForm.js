e90617c3285fa704978eea34ddf655281e3691ad
Om Bikram Rana <ome.bikram@gmail.com>
2023-04-17 17:43:47 +0200

    CSS

--- dentistbookingapp/src/BookingForm.js
+++ dentistbookingapp/src/BookingForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './BookingForm.css';
 
 class BookingForm extends React.Component {
   state = {
@@ -39,23 +40,36 @@ class BookingForm extends React.Component {
   toggleCalendar = () => {
     this.setState({ showCalendar: !this.state.showCalendar });
   };
+handleSubmit = event => {
+    alert("booking completed");
 
+}
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Date:
-          <input type="text" value={this.state.date} onClick={this.toggleCalendar} readOnly />
-          {this.state.showCalendar && <Calendar value={this.state.date} onChange={this.handleDateChange} />}
-        </label>
-        <br />
-        <label>
-          Time:
-          <input type="time" value={this.state.time} onChange={this.handleTimeChange} />
-        </label>
-        <br />
-        <button type="submit">Book Appointment</button>
-      </form>
+        <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <h1>IT Offfer Dental Appoinment Booking System</h1>
+            <form className="booking-form" onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="date-input">Date:</label>
+                <div className="input-group">
+                  <input id="date-input" type="text" value={this.state.date} onClick={this.toggleCalendar} readOnly className="form-control" />
+                  <div className="input-group-append">
+                    <span className="input-group-text"><i className="fas fa-calendar-alt"></i></span>
+                  </div>
+                </div>
+                {this.state.showCalendar && <Calendar className="calendar" value={this.state.date} onChange={this.handleDateChange} />}
+              </div>
+              <div className="form-group">
+                <label htmlFor="time-input">Time:</label>
+                <input id="time-input" type="time" value={this.state.time} onChange={this.handleTimeChange} className="form-control" />
+              </div>
+              <button type="submit" className="btn btn-primary btn-block">Book Appointment</button>
+            </form>
+          </div>
+        </div>
+      </div>
     );
   }
 }
